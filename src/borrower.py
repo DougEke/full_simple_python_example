9b9bf1df7cf4305f0f73a646ed920a0e6135eaeb
DougEke <95352026+DougEke@users.noreply.github.com>
2023-06-30 16:50:37 +0100

    Modified the Borrower class

--- src/borrower.py
+++ src/borrower.py
@@ -7,29 +7,47 @@ Book    : A Book object that holds the details of a specific book
 """
 
 from dataclasses import dataclass
+from datetime import date
+
 from book import Book
 from person import Person
 
 
 @dataclass
 class Borrower:
+    person: Person
+    book: Book
+    loan_date: date
+
     def __init__(self, person: Person, book: Book, date):
-        self.__person = person
-        self.__book = book
-        self.__date = date
+        self.person = person
+        self.book = book
+        self.loan_date = date
 
-    @property
-    def person(self):
-        return self.__person
-
-    @property
-    def book(self):
-        return self.__book
-
-    @property
-    def date(self):
-        return self.__date
-
-    # Display the information of the borrower...
+    # # Display the information of the borrower...
     def borrowerInfo(self):
-        return f"\n***Borrower***\nDate: {self.date}\nName: {self.person}\nBook: {self.book}"
+        return f"\n***Borrower***\nDate: {self.loan_date}\nName: {self.person}\nBook: {self.book}"
+
+
+# @dataclass
+# class Borrower:
+#     def __init__(self, person: Person, book: Book, date):
+#         self.__person = person
+#         self.__book = book
+#         self.__date = date
+
+#     @property
+#     def person(self):
+#         return self.__person
+
+#     @property
+#     def book(self):
+#         return self.__book
+
+#     @property
+#     def date(self):
+#         return self.__date
+
+#     # Display the information of the borrower...
+#     def borrowerInfo(self):
+#         return f"\n***Borrower***\nDate: {self.date}\nName: {self.person}\nBook: {self.book}"
